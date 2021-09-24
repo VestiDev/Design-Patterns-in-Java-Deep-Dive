0e0d225bf803bced7c9798df76a71eb00d6ef381
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-09-24 17:14:36 +0200

    Initialize repository

---
+++ src/composite/solution1/Contact.java
@@ -0,0 +1,28 @@
+/*
+ * This class forms part of the Design Patterns Course by
+ * Dr Heinz Kabutz from JavaSpecialists.eu and may not be
+ * distributed without written consent.
+ *
+ * Copyright 2001-2018, Heinz Kabutz, All rights reserved.
+ */
+package composite.solution1;
+
+import java.util.*;
+
+public interface Contact {
+    default void add(Contact contact) {
+    }
+
+    default void remove(Contact contact) {
+    }
+
+    void sendMail(String msg);
+
+    default boolean isLeaf() {
+        return true;
+    }
+
+    default Iterator<Contact> children() {
+        return Collections.emptyIterator();
+    }
+}
